a535f51206841cab2271ee4a424238c645710402
Christopher <voschristopher53@gmail.com>
2024-06-24 09:35:25 +0200

    added mouseEffect to hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,14 @@
 
+import MouseEffect from "@/components/MouseEffect";
 import Hero from "./components/Hero";
 import styles from "./home.module.css";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <Hero />
+      <MouseEffect distance={85} icon="bi-star-fill">
+        <Hero />
+      </MouseEffect>
     </main>
   );
 }
